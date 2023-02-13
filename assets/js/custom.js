e29971e536614321c9006d4f870eba765ae5b8da
tony_ <antonyrebolledo@gmail.com>
2023-02-13 12:11:09 -0600

    Actividades 13-02

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -106,4 +106,17 @@
     });
 
 
+    //show and hide password
+    const togglePassword = document.querySelector('#togglePassword');
+    const password = document.querySelector('#id_password');
+
+    togglePassword.addEventListener('click', function (e) {
+        // toggle the type attribute
+        const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+        password.setAttribute('type', type);
+        // toggle the eye slash icon
+        this.classList.toggle('fa-eye');
+    });
+
+
 }(jQuery));
